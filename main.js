42dc06be1ac87167c688e0f8f728b409496b9c64
Ramon Alejandro Aguirre Romero <73568821+LexAguirre@users.noreply.github.com>
2022-09-03 17:39:10 -0500

    Moved

---
+++ main.js
@@ -0,0 +1,35 @@
+//operadores aritmeticos 
+//suma
+console.log(5 + 7);
+//resta
+console.log(5 - 7);
+//multiplicacion
+console.log(5 * 7);
+//potenciacion
+console.log(2 ** 3);
+//divicion
+console.log(5 / 2);
+//residuo de la division 
+console.log(5 % 2);
+console.log(7 % 3);
+
+//operadores de asignacion 
+let a = 5 + 2;
+
+console.log(a);
+
+//sumarle a la variable anterior 
+a = a + 1; 
+console.log(a);
+
+//forma avrebiada de decir lo anterior sumarle 1
+a++;
+
+console.log(a);
+
+a = a - 1; 
+console.log(a);
+
+//este es el mismo caso solo que restarle 1
+a--;
+console.log(a);
